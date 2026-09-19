f6de840f71019c7f21209d2c830653bcd59a5e62
Junichi Motohashi <junichi.m.2020@gmail.com>
2026-09-19 23:56:22 +0900

    Switch to reliable Agoda affiliate URL with CID top page landing

--- data/hotels.ts
+++ data/hotels.ts
@@ -146,8 +146,8 @@ export const bangkokHotels: HotelItem[] = [
 const AGODA_CID = '1974942';
 
 /**
- * キーワードからAgodaの検索用アフィリエイトURLを生成する関数
+ * 確実なCID付きAgodaトップページURLを生成する関数
  */
-export const getAgodaSearchUrl = (keyword: string) => {
-    return `https://www.agoda.com/search?q=${encodeURIComponent(keyword)}&cid=${AGODA_CID}&hl=ja`;
+export const getAgodaSearchUrl = (_keyword?: string) => {
+    return `https://www.agoda.com/ja-jp/?cid=${AGODA_CID}`;
 };
