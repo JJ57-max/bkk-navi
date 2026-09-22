288059ecd0e3638d655a71d0b52d4c1a9d542b80
Junichi Motohashi <junichi.m.2020@gmail.com>
2026-09-22 10:39:52 +0900

    feat:  add target URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata, Viewport } from "next";
 import { GoogleAnalytics } from '@next/third-parties/google';
-import Script from 'next/script'; // 追加
+import Script from 'next/script';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -40,6 +40,12 @@ export default function RootLayout({
       <Script id="impact-tracking" strategy="afterInteractive">
         {`(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7822724-1d97-417f-8714-920efb4f56a01.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`}
       </Script>
+
+      {/* バリューコマース LinkSwitchタグ */}
+      <Script id="valuecommerce-linkswitch" strategy="afterInteractive">
+        {`var vc_pid = "89270986";`}
+      </Script>
+      <Script src="//aml.valuecommerce.com/vcdal.js" strategy="afterInteractive" />
     </html>
   );
 }
